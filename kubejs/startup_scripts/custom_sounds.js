d7f84c0a60b65729b6de57acf43f7411a7ff2d85
laforetbrut <vyrriox@gmail.com>
2026-03-19 15:52:40 +0100

    feat: ajout du CD Au Pactole

--- kubejs/startup_scripts/custom_sounds.js
+++ kubejs/startup_scripts/custom_sounds.js
@@ -11,7 +11,7 @@ StartupEvents.registry('sound_event', event => {
         'vyrriox_le_bouffeur_de_patee', 'vyrriox_la_naine_pt1', 'vyrriox_la_naine_pt2', 'vyrriox_la_naine_pt3',
         'vyrriox_la_femme_de_joie', 'peter_le_frein_pt1', 'peter_le_frein_pt2', 'peter_le_frein_pt3',
         'peter_le_frein_pt4', 'peter_le_frein_pt5', 'boit_ton_picher', 'dans_la_tavern_lulu',
-        'janette', 'la_boulette_pt1', 'la_boulette_pt2'
+        'janette', 'la_boulette_pt1', 'la_boulette_pt2', 'au_pactole'
     ];
 
     sounds.forEach(id => {
