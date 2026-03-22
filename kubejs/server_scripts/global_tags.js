82d91796dda8dfce5c36349644ad338a27798169
laforetbrut <vyrriox@gmail.com>
2026-03-22 09:42:31 +0100

    refactor: refine global tags to use standard c: categories

--- kubejs/server_scripts/global_tags.js
+++ kubejs/server_scripts/global_tags.js
@@ -1,106 +1,66 @@
 // Priority: 10
 /*
-    Global Tags for Item Sorting & Organization
-    Adds common and custom tags to various items to facilitate sorting in storage systems.
+    Global Tags for Item Sorting & Organization (Refined)
+    Adds standard and descriptive tags to various items.
     Author: vyrriox
 */
 
 ServerEvents.tags('item', event => {
-    // 1. Netherite
-    event.add('c:netherite', [
+    // 1. Netherite & Valuables
+    event.add('c:netherite_scraps', 'minecraft:netherite_scrap');
+    event.add('c:smithing_templates', 'minecraft:netherite_upgrade_smithing_template');
+    event.add('c:valuables', [
         'minecraft:netherite_scrap',
+        'minecraft:totem_of_undying',
+        'minecraft:ender_eye',
         'minecraft:netherite_upgrade_smithing_template'
     ]);
 
     // 2. Ender
-    event.add('c:ender', 'minecraft:ender_eye');
+    event.add('c:ender_eyes', 'minecraft:ender_eye');
 
-    // 3. Flint
-    event.add('c:flint', 'minecraft:flint');
+    // 3. Minerals & Flint
+    event.add('c:flints', 'minecraft:flint');
+    event.add('c:smooth_basalts', 'minecraft:smooth_basalt');
 
-    // 4. Kelp
-    event.add('c:kelp', 'minecraft:kelp');
+    // 4. Flora
+    event.add('c:kelps', 'minecraft:kelp');
+    event.add('c:grasses', ['minecraft:short_grass', 'minecraft:tall_grass']);
 
-    // 5. Mob Drops
-    event.add('c:mob_drops', [
-        'minecraft:magma_cream',
-        'minecraft:blaze_powder',
-        'minecraft:phantom_membrane'
-    ]);
+    // 5. Mob Drops (Specific)
+    event.add('c:mob_drops/magma', 'minecraft:magma_cream');
+    event.add('c:mob_drops/blaze', 'minecraft:blaze_powder');
+    event.add('c:mob_drops/phantom', 'minecraft:phantom_membrane');
 
     // 6. Projectiles
-    event.add('c:projectiles', [
-        'minecraft:fire_charge',
-        'minecraft:wind_charge'
-    ]);
+    event.add('c:projectiles/fire', 'minecraft:fire_charge');
+    event.add('c:projectiles/wind', 'minecraft:wind_charge');
 
-    // 7. Grasses
-    event.add('c:grasses', [
-        'minecraft:short_grass',
-        'minecraft:tall_grass'
-    ]);
+    // 7. Utility & Tools
+    event.add('c:honeycombs', 'minecraft:honeycomb');
+    event.add('c:glistering_melons', 'minecraft:glistering_melon_slice');
+    event.add('c:experience_bottles', 'minecraft:experience_bottle');
+    event.add('c:name_tags', 'minecraft:name_tag');
+    event.add('c:trial_keys', 'minecraft:trial_key');
+    event.add('c:totems', 'minecraft:totem_of_undying');
 
-    // 8. Honeycomb
-    event.add('c:honeycomb', 'minecraft:honeycomb');
+    // 8. Currency (Dusty Decorations)
+    event.add('c:currencies/gold_coins', 'dustydecorations:scattered_gold_coins');
 
-    // 9. Glistering Melon
-    event.add('c:glistering_melon', 'minecraft:glistering_melon_slice');
-
-    // 10. Magic & Valuable
-    event.add('c:magic', [
-        'minecraft:experience_bottle',
-        'minecraft:totem_of_undying'
-    ]);
-
-    // 11. Utility
-    event.add('c:utility', [
-        'minecraft:name_tag',
-        'minecraft:trial_key'
-    ]);
-
-    // 12. Smooth Basalt
-    event.add('c:smooth_basalt', 'minecraft:smooth_basalt');
-
-    // 13. Currency
-    event.add('c:currency', 'dustydecorations:scattered_gold_coins');
-    event.add('arcadia:is_item', 'dustydecorations:scattered_gold_coins'); // Grouping with other custom items
-
-    // 14. Redstone Components
+    // 9. Redstone & Pistons
+    event.add('c:pistons', ['minecraft:sticky_piston', 'minecraft:piston']);
+    event.add('c:levers', 'minecraft:lever');
     event.add('c:redstone_components', [
-        'minecraft:sticky_piston',
-        'minecraft:piston',
-        'minecraft:lever',
         'minecraft:comparator',
         'minecraft:repeater'
     ]);
-
-    // 15. General Grouping (requested by user)
-    event.add('arcadia:is_item', [
-        'minecraft:netherite_scrap',
-        'minecraft:ender_eye',
-        'minecraft:flint',
-        'minecraft:kelp',
-        'minecraft:magma_cream',
-        'minecraft:blaze_powder',
-        'minecraft:fire_charge',
-        'minecraft:phantom_membrane',
-        'minecraft:wind_charge',
-        'minecraft:short_grass',
-        'minecraft:tall_grass',
-        'minecraft:honeycomb',
-        'minecraft:glistering_melon_slice',
-        'minecraft:experience_bottle',
-        'minecraft:name_tag',
-        'minecraft:trial_key',
-        'minecraft:netherite_upgrade_smithing_template',
-        'minecraft:totem_of_undying',
-        'minecraft:smooth_basalt',
-        'minecraft:sticky_piston',
+    event.add('c:redstone_devices', [
         'minecraft:piston',
+        'minecraft:sticky_piston',
         'minecraft:lever',
         'minecraft:comparator',
         'minecraft:repeater'
     ]);
 });
 
-console.info("[Arcadia V2] Global Tags for Sorting Loaded Successfully.");
+console.info("[Arcadia V2] Global Tags Refined: Standardized tags applied.");
