20ae482f39cc186e731f3e74e6efcb613991fd3c
laforetbrut <vyrriox@gmail.com>
2026-05-23 21:46:52 +0200

    Revert "ban(createcasing): disable mechanical saw casings - broken belt I/O"
    This reverts commit c465ea2015dbc510a516278de8ed7f7e8d77233a.

--- kubejs/server_scripts/items/banned/recipe_remover.js
+++ kubejs/server_scripts/items/banned/recipe_remover.js
@@ -197,17 +197,7 @@ ServerEvents.recipes(event => {
     'dndesires:gold_mixer',
 
     // Twilight Forest (Uncrafting Table - allows free item duplication exploit)
-    'twilightforest:uncrafting_table',
-
-    // Create Encased (Mechanical Saw casings - broken belt I/O, items get stuck)
-    'createcasing:brass_mechanical_saw',
-    'createcasing:copper_mechanical_saw',
-    'createcasing:creative_mechanical_saw',
-    'createcasing:industrial_iron_mechanical_saw',
-    'createcasing:railway_mechanical_saw',
-    'createcasing:refined_radiance_mechanical_saw',
-    'createcasing:shadow_steel_mechanical_saw',
-    'createcasing:weathered_iron_mechanical_saw'
+    'twilightforest:uncrafting_table'
   ];
 
   // Disable vanilla item repair recipe (prevents dual-wield weapons from merging)
